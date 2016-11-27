c5928d0fe3b7ce0f79b45aa178092877650b09b4
Subhdeep Saha <subhdeepsaha@gmail.com>
2016-11-27 12:34:20 +0530

    form post request done

--- server.js
+++ server.js
@@ -7,35 +7,18 @@ var Student = require('./models/student');
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-var router = express.Router();
 mongoose.connect('mongodb://localhost/admission');
 
-router.use(function(req,res,next) {
-  console.log("Something is Happening");
-  next();
-});
-
-router.get('/home', function(req, res) {
+app.get('/home', function(req, res) {
   res.json({message : 'routing is working'});
 });
 
 
-router.route('/newstud')
-  .post(function(req,res) {
-    console.log(req.body.first_name);
-    var student = new Student();
-    student.first_name = req.body.first_name;
-
-    student.save(function(err) {
-      if(err) {
-        res.send(err);
-      } else {
-        res.json({message : 'Student created'});
-      }
-    });
+app.post('/newstud', function(req, res) {
+    console.log("Request processed");
+    console.log(req.body);
   });
 
-app.use('/api', router);
 app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000, function() {
