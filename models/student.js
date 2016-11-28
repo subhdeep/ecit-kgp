a16b857101f44861e2e36de08108f4cac0651d1f
Subhdeep Saha <subhdeepsaha@gmail.com>
2016-11-28 10:13:20 +0530

    completed newstud model

--- models/student.js
+++ models/student.js
@@ -3,7 +3,22 @@ var Schema = mongoose.Schema;
 
 var studSchema = new Schema({
   first_name: String,
-  last_name: String
+  last_name: String,
+  address: String,
+  father_name: String,
+  male: String,
+  female: String,
+  telephone: String,
+  birthday: Date,
+  postal_code: String,
+  passyear: String,
+  institution: String,
+  degree: String,
+  marks: String,
+  passyear1: String,
+  institution1: String,
+  course: String,
+  marks1: String
 });
 
 var Student = mongoose.model('Student', studSchema);
