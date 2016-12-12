3743ce3a3752c0acc8bd1b955da6c9fda5c7af28
dart-subhdeep <subhdeep>
2016-12-12 12:12:45 +0530

    form validation

--- public/admission/script/script.js
+++ public/admission/script/script.js
@@ -2,7 +2,7 @@ $("document").ready(function() {
   $("#dob").click(function(){
     $('.datepicker').pickadate({
       selectMonths: true, // Creates a dropdown to control month
-      selectYears: 100 // Creates a dropdown of 15 years to control year
+      selectYears: 200 // Creates a dropdown of 15 years to control year
     });
   });
 });
